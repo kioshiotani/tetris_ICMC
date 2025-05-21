8671591f19bf6dab6aa9a6830b1a41fcb3d22a20
kioshiotani <brunokioshiotani@gmail.com>
2025-05-21 14:17:39 -0300

    Atualização do planejamento

---
+++ tetris2.asm
@@ -0,0 +1,38 @@
+jmp main
+
+;mapa
+mapa0  : string "                                        "
+mapa1  : string "                                        "
+mapa2  : string "                                        "
+mapa3  : string "                                        "
+mapa4  : string "                                        "
+mapa5  : string "                                        "
+mapa6  : string "                                        "
+mapa7  : string "                                        "
+mapa8  : string "                                        "
+mapa9  : string "                                        "
+mapa10 : string "                                        "
+mapa11 : string "                                        "
+mapa12 : string "                                        "
+mapa13 : string "                                        "
+mapa14 : string "                                        "
+mapa15 : string "                                        "
+mapa16 : string "                                        "
+mapa17 : string "                                        "
+mapa18 : string "                                        "
+mapa19 : string "                                        "
+mapa20 : string "                                        "
+mapa21 : string "                                        "
+mapa22 : string "                                        "
+mapa23 : string "                                        "
+mapa24 : string "                                        "
+mapa25 : string "                                        "
+mapa26 : string "                                        "
+mapa27 : string "                                        "
+mapa28 : string "                                        "
+mapa29 : string "                                        "
+
+;posicoes
+pos     : var #1
+pos_ant : var #1
+
